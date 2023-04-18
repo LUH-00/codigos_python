a0b2a0f86edde0fa0bfe577964afe7494cabae98
unknown <luan@gmail.com>
2023-04-17 21:02:39 -0300

    Criado aula 2 em Python

---
+++ aula 2.py
@@ -0,0 +1,18 @@
+nome = input ("Informe seu nome: ")
+idade = input ("informe sua idade: " )
+
+#Isso é um comentário de uma linha 
+"""
+Isso é um comentário de várias linhas 
+
+"""
+# Primeira forma de concatenar 
+
+print ("Olá  ",nome,",sua idade é ", idade, "anos")
+
+print ("Olá {} sua idade é {}".format(nome,idade))
+print (f"Olá {nome} sua idade é {idade}")
+
+
+
+
