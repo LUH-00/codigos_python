94721a4c7c4664c0158217974cba18f4b12d243f
unknown <luan@gmail.com>
2023-04-20 20:49:47 -0300

    Criado aula 5 sobre condicional em Pytho

---
+++ aula 5 condicional.py
@@ -0,0 +1,11 @@
+nota1 = float(input("Informe a 1º nota: "))
+nota2 = float(input("Informe a 2º nota: "))
+
+media = (nota1 + nota2) / 2
+
+if media >= 7: # SE 
+    print("Você passou :)")
+elif media == 6:  # 
+    peint ("Você está de recuperação")
+else: # SENAO
+    print("Você ficou reprvado :(")
