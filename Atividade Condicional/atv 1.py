5e7f21461fef1f91b0662b81eb4b82d8566f6918
unknown <luan@gmail.com>
2023-04-20 21:07:43 -0300

    p atv condicional

---
+++ Atividade Condicional/atv 1.py
@@ -0,0 +1,6 @@
+valor = int(input("Informe um valor qualquer: "))
+
+if valor >= 100 and valor <= 200:   # END essa condição e aquela tem que ser verdade 
+    print(f"O valor {valor} está entre 100 e 200 \n")
+else:
+    print(f"O valor {valor} não está entre 100 e 200 \n")
