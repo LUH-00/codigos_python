dee2ce55860cfa9a1da97c25b504de45685c1683
unknown <luan@gmail.com>
2023-04-24 19:44:30 -0300

    atv condicional 06

---
+++ Atividade Condicional/atv 6.py
@@ -0,0 +1,17 @@
+percurso = float(input("Informe o percurso em Km: "))
+tipoCarro = int(input("Informe o tipo do carro, 1, 2 ou 3: "))
+
+if tipoCarro == 1: #== verdade absoluta 
+    total = percurso / 8
+    print (f"O tipo do carro é {tipoCarro} vai precisar de {total} litros de gasolina \n")                                                   #pass  pra não coçocar conteudo
+elif tipoCarro == 2:
+    total= percurso / 9
+    print (f"O tipo do carro é {tipoCarro} vai precisar de {total} litros de gasolina \n")
+    
+elif tipoCarro == 3:
+    total= percurso / 12
+    print (f"O tipo do carro é {tipoCarro} vai precisar de {total} litros de gasolina \n")
+    
+
+else:
+    print("Tipo carro inválido \n") 
