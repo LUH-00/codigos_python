9c450013ea683c8ace9d18f2cbf7f413ccd91e87
unknown <luan@gmail.com>
2023-04-24 21:01:01 -0300

    Aula 06 em Python (PARA)

---
+++ aula 6 para.py
@@ -0,0 +1,5 @@
+for contador in range (1,11): #contagem do menor até i maior  | o 3 parametro é pra mostra a quantidade de casa que ele pula 
+    print (contador)
+
+for contador in range (11, 0, -1): # do maior para o menor coloca o -1
+    print (contador) 
