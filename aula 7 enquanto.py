40fa2fbfc5f6afd91ae8d0d63313109c4e0d2cbe
unknown <luanvmc.000@gmail.com>
2023-05-02 21:13:05 -0300

    Iniciado  aula 07 (while) em python

---
+++ aula 7 enquanto.py
@@ -0,0 +1,21 @@
+# while funcionando como loop contado 
+contador = 1
+
+while contador <= 5:
+     print(contador)
+     contador = contador + 1
+
+# while  funcionando com loop condicional 
+"""
+senha  = ""
+while senha != "123":
+     senha =  input("Informe sua senha: ")
+
+print("Obrigado por usar o sistema\n")
+"""
+# While como se fosse o faça - enquanto
+
+while True: 
+     senha = input("informe sua senha: ")
+     if senha == "123":
+        break 
