99863de203fb0065d5fc630960d2f93478b27d0e
unknown <luanvmc.000@gmail.com>
2023-05-08 20:25:49 -0300

    Criado aula 08 TUPLA em python

---
+++ aula 8-tupla.py
@@ -0,0 +1,22 @@
+lanche = ("Pizza", "Hotdog" , "Refri", "Batata")
+print (lanche)
+print(type(lanche)) # estou mostrando o tipo da variavel 
+
+print(lanche[1])
+print(f"O total de lanches é {len(lanche)} \n") #  length
+
+#lanche{2} = "Suco"
+
+for contador in range (0, 4):
+    print(lanche[contador])
+
+print("-"*30)
+
+for item in lanche:
+    print(item)
+
+print("-"*30)
+#enumenrate serve para permitir acessar os índices da tupla. Já a variavel, ira armazenar os valores do indice
+for indice,elemento in enumerate(lanche):
+    print (f"{indice} = {elemento}")
+    
