9afd3d124c5075615ba7542b4d2910276943f6c1
unknown <luanvmc.000@gmail.com>
2023-05-09 21:05:58 -0300

    Iniciado atividade LISTA

---
+++ Atividade Lista/atv llista 01.py
@@ -0,0 +1,16 @@
+numeros = []
+valor = 1
+impa = 0 
+pa = 0
+for contador in range (1,11):
+    numeros.append(contador)
+print(numeros)
+for cont in range (1,11):
+    if pa  % 2 == 0:
+        print (f"os numeros pares são {pa}")
+    if impa % 2 != 0: 
+        print(f"Os numeros impares são {impa}")
+    
+    
+
+
