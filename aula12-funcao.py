1990d545780fbbf0163a51d835f5b1d55bc46057
RFT AL <rftal@fiema.org.br>
2023-05-17 20:06:26 -0300

    Finalizado aula de FUNÇÃO em python

---
+++ aula12-funcao.py
@@ -0,0 +1,17 @@
+
+# FUNÇÃO SEM PARÂMETRO (O def é de definição )
+def somar():
+    valor1 = int(input("Informe um valor: "))
+    valor2 = int(input("Informe outro valor: "))
+
+    print(valor1 + valor2)
+
+soma()
+#FUNÇÃO COM PARÂMETRO 
+num1 = int(input("Informe o valor 1: "))
+num2 = int(input("Informe o valor 2: "))
+
+def multiplicar():
+    print(num1 * num2)
+
+
---
+++ aula12.1-funcao-cont.py
@@ -0,0 +1,8 @@
+#FUNÇÃO COM RETORNO
+nome = input("Informe seu nome: ")
+
+def contarLetra(texto):
+    #print(f" O nome possui o total de 0{len(texto)} letras")
+    return len(texto)
+
+print (contarLetra(texto))
